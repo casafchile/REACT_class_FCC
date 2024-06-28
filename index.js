3264e7d7eb96f10ba538e1f3a64cc7de1dbb172f
Felipe Herrera <83690907+casafchile@users.noreply.github.com>
2024-06-28 16:28:22 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const JSX = (
+  <div className="myDiv">   //clase en REACT es con el atributo className en vez de class de html
+                            //y para usarlo se puede usar onClick onChange 
+    <h1>Add a class to this div</h1>
+  </div>
+);
